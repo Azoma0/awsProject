13971541a953f551f666758eed8faa49f66d9044
azoma <azoma@d.com>
2025-05-21 15:25:03 +0500

    five commit

--- src/App.vue
+++ src/App.vue
@@ -14,20 +14,16 @@ body {
     <textarea v-model="reviewText" placeholder="Введите текст..." rows="10" cols="100"/>
     <br />
     <button @click="submitReview">Отправить</button>
-
-    
-    
-  </div>
-  <div>
+<div>
     <h2 >Эмоциональная характеристика</h2>
     <li class="status" style="height: 50px; width: 50px;">
 
     </li>
   </div>
+  </div>
+    
+    
 
-<div>
-  <area shape="0" coords="10" href="" alt="status">
-</div>
   
 </template>
 
